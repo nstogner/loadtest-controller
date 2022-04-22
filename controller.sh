6a2bcd1f549f1875865bd1d5f2ebd41e97404299
Nick Stogner <nicholas.stogner@gmail.com>
2022-04-22 17:03:36 -0400

    Change ConfigMap to LoadTest custom resource

--- controller.sh
+++ controller.sh
@@ -5,28 +5,28 @@ reconcile () {
 	name=$2
 
 	echo "[$namespace/$name] Reconciling"
-	loadtest=$(kubectl get configmap -ojson -n $namespace $name)
+	loadtest=$(kubectl get loadtest -ojson -n $namespace $name)
 
-	ltlabel=$(echo $loadtest | jq -r '.metadata.labels.loadtest')
-	[[ "$ltlabel" != "yes" ]] && echo "Not loadtest, ignoring" && exit 0
+	#ltlabel=$(echo $loadtest | jq -r '.metadata.labels.loadtest')
+	#[[ "$ltlabel" != "yes" ]] && echo "Not loadtest, ignoring" && exit 0
 
-	requests=$(echo $loadtest | jq -r '.data.requests')
+	requests=$(echo $loadtest | jq -r '.status.requests')
 	[[ "$requests" != "null" ]] && echo "Load test already ran, ignoring" && exit 0
 
-	method=$(echo $loadtest | jq -r '.data.method')
-	address=$(echo $loadtest | jq -r '.data.address')
-	duration=$(echo $loadtest | jq -r '.data.duration')
+	method=$(echo $loadtest | jq -r '.spec.method')
+	address=$(echo $loadtest | jq -r '.spec.address')
+	duration=$(echo $loadtest | jq -r '.spec.duration')
 
 	echo "[$namespace/$name] Run load test (method=$method, address=$address, duration=$duration)"
 	results=$(echo "$method $address" | vegeta attack --duration=$duration | vegeta report --type json | jq)
 
 	requests=$(echo $results | jq -r '.requests')
 	echo "[$namespace/$name] Requests = $requests"
-	kubectl patch configmap -n $namespace $name --type='json' --patch='[{"op": "replace", "path": "/data/requests", "value": "'$requests'"}]'
+	kubectl patch loadtest -n $namespace $name --type='json' --patch='[{"op": "replace", "path": "/status", "value": {"requests":'$requests'}}]'
 }
 
 # The function needs to be exported so that is can be called with xargs.
 export -f reconcile
 
-kubectl get configmaps --all-namespaces --watch --output go-template='{{printf "%s %s\n" .metadata.namespace .metadata.name}}' | xargs -L1 bash -c 'reconcile "$@"' _
+kubectl get loadtest --all-namespaces --watch --output go-template='{{printf "%s %s\n" .metadata.namespace .metadata.name}}' | xargs -L1 bash -c 'reconcile "$@"' _
 
